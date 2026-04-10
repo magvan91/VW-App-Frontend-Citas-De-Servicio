8a50ede87460d9699badc0ca7f0d48939172718e
Marco García <marcoantonio.garcia@vml.com>
2026-04-09 21:50:37 -0600

    Add file interface CitasServicioValues

---
+++ src/interfaces/CitasServicioValues.interface.ts
@@ -0,0 +1,22 @@
+export interface CitasServicioValues {
+  numeroChasis: string;
+  anio: string;
+  modelo: string;
+  kilometrajeAuto: string | number;
+  kilometrajeServicio: string;
+  idConcesionario: string;
+  estado: string;
+  ciudad: string;
+  dates: [Date, Date] | null;
+  fecha: string;
+  horario: string;
+  nombre: string;
+  apePat: string;
+  apeMat: string;
+  telefonoMovil: string;
+  email: string;
+  comentarios: string;
+  aceptaAviso: boolean;
+  opt_in_transferencia_datos: boolean;
+  tyco: boolean;
+}
